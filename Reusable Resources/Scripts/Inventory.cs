4624ebbbdffde4b06c07f7f3e2d18123e631d06e
Utku Gokalp <38686664+UtkuGokalp@users.noreply.github.com>
2021-01-27 15:18:23 +0300

    Inventory script updated.
    Fixed bugs and improved memory usage.

--- Reusable Resources/Scripts/Inventory.cs
+++ Reusable Resources/Scripts/Inventory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Utility.Development;
 using System.Collections.Generic;
 
 namespace Utility.Inventory
@@ -6,21 +7,42 @@ namespace Utility.Inventory
     public class Inventory<TItemBase> : IEnumerable<TItemBase>
     {
         #region Variables
-        private readonly List<List<TItemBase>> inventory;
+#if UNITY_EDITOR
+        [UnityEngine.SerializeField] //Debug attribute
+#endif
+        private readonly List<TItemBase>[] inventory;
         private readonly IEqualityComparer<TItemBase> itemEqualityComparer;
+        public event TypeSafeEventHandler<Inventory<TItemBase>, OnItemAddedEventArgs> OnItemAdded;
+        public event TypeSafeEventHandler<Inventory<TItemBase>, OnItemRemovedEventArgs> OnItemRemoved;
         #endregion
 
         #region Constructor
         /// <summary>
         /// Creates a new inventory and initializes it with the given starting items where key is the item and value is the item count.
         /// </summary>
-        public Inventory(IEqualityComparer<TItemBase> itemEqualityComparer, params KeyValuePair<TItemBase, int>[] startingItems)
+        /// <param name="startingItems">If length of startingItems array is bigger than inventorySize, only items that fit into the inventory are added to the inventory.</param>
+        public Inventory(int inventorySize, IEqualityComparer<TItemBase> itemEqualityComparer, params KeyValuePair<TItemBase, int>[] startingItems)
         {
             this.itemEqualityComparer = itemEqualityComparer;
-            inventory = new List<List<TItemBase>>();
-            foreach (KeyValuePair<TItemBase, int> pair in startingItems)
+            inventory = new List<TItemBase>[inventorySize];
+            for (int i = 0; i < inventory.Length; i++)
             {
-                AddItem(pair.Key, pair.Value);
+                inventory[i] = new List<TItemBase>();
+            }
+
+            if (startingItems.Length > inventory.Length)
+            {
+                for (int i = 0; i < inventory.Length; i++)
+                {
+                    AddItem(startingItems[i].Key, startingItems[i].Value);
+                }
+            }
+            else
+            {
+                foreach (KeyValuePair<TItemBase, int> pair in startingItems)
+                {
+                    AddItem(pair.Key, pair.Value);
+                }
             }
         }
         #endregion
@@ -29,13 +51,7 @@ namespace Utility.Inventory
         /// <summary>
         /// Returns the item at the given index.
         /// </summary>
-        public TItemBase this[int index]
-        {
-            get
-            {
-                return inventory[index][inventory[index].Count - 1];
-            }
-        }
+        public TItemBase this[int index] => inventory[index][inventory[index].Count - 1];
         #endregion
 
         #region AddItem
@@ -44,6 +60,7 @@ namespace Utility.Inventory
         /// </summary>
         public void AddItem(TItemBase item, int amount = 1)
         {
+        startAddingItems:
             if (ContainsItem(item, out int index, out int _))
             {
                 for (int i = 0; i < amount; i++)
@@ -53,11 +70,14 @@ namespace Utility.Inventory
             }
             else
             {
-                for (int i = 0; i < amount; i++)
-                {
-                    inventory.Add(new List<TItemBase> { item });
-                }
+                //Add one item, decrease amount by one and hand over 
+                //the addition logic to the piece of code which is 
+                //responsible for adding items that are already in the inventory.
+                inventory[(int)GetFirstAvailableSlotIndex()].Add(item);
+                amount--;
+                goto startAddingItems;
             }
+            OnItemAdded?.Invoke(this, new OnItemAddedEventArgs(amount, index, item));
         }
         #endregion
 
@@ -77,11 +97,7 @@ namespace Utility.Inventory
                     inventory[index].RemoveAt(inventory[index].Count - 1);
                     removedAmount++;
                 }
-
-                if (inventory[index].Count == 0)
-                {
-                    inventory.RemoveAt(index);
-                }
+                OnItemRemoved?.Invoke(this, new OnItemRemovedEventArgs(removedAmount, index, item));
                 return true;
             }
             return false;
@@ -92,11 +108,11 @@ namespace Utility.Inventory
         /// </summary>
         public bool RemoveItem(int index, int amount = 1)
         {
-            if (inventory.Count == 0)
+            if (!ContainsAnyItem())
             {
                 return false;
             }
-            if (index > inventory.Count - 1)
+            if (index > inventory.Length - 1)
             {
                 throw new System.IndexOutOfRangeException();
             }
@@ -106,13 +122,13 @@ namespace Utility.Inventory
 
         #region GetItemCount
         /// <summary>
-        /// Returns the given item count.
+        /// Returns the count of the given item.
         /// </summary>
         public int GetItemCount(TItemBase item)
         {
-            for (int i = 0; i < inventory.Count; i++)
+            for (int i = 0; i < inventory.Length; i++)
             {
-                if (itemEqualityComparer.Equals(inventory[i][0], item))
+                if (inventory[i].Count != 0 && itemEqualityComparer.Equals(inventory[i][0], item))
                 {
                     return inventory[i].Count;
                 }
@@ -120,9 +136,12 @@ namespace Utility.Inventory
             return 0;
         }
 
+        /// <summary>
+        /// Returns the item count at the given index.
+        /// </summary>
         public int GetItemCount(int index)
         {
-            if (index <= inventory.Count - 1)
+            if (index <= inventory.Length - 1)
             {
                 return inventory[index].Count;
             }
@@ -130,24 +149,83 @@ namespace Utility.Inventory
         }
         #endregion
 
+        #region GetTotalItemCount
+        /// <summary>
+        /// Returns the number of total items this inventory has.
+        /// </summary>
+        public int GetTotalItemCount()
+        {
+            int totalItemCount = 0;
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i].Count != 0)
+                {
+                    totalItemCount++;
+                }
+            }
+            return totalItemCount;
+        }
+        #endregion
+
         #region ContainsItem
         /// <summary>
         /// Returns whether the given item exists or not and sets the index and the count varible if exists. Index is set to -1 and count to 0 if the item doesn't exist.
         /// </summary>
-        public bool ContainsItem(TItemBase item, out int index, out int count)
+        public bool ContainsItem(TItemBase item, out int itemIndexInInventory, out int itemCount)
         {
-            for (int i = 0; i < inventory.Count; i++)
+            for (int i = 0; i < inventory.Length; i++)
             {
-                if (itemEqualityComparer.Equals(inventory[i][0], item))
+                if (inventory[i].Count != 0 && itemEqualityComparer.Equals(inventory[i][0], item))
                 {
-                    index = i;
-                    count = inventory[i].Count;
+                    itemIndexInInventory = i;
+                    itemCount = inventory[i].Count;
                     return true;
                 }
             }
-            index = -1;
-            count = 0;
+            itemIndexInInventory = -1;
+            itemCount = 0;
             return false;
+        }
+        #endregion
+
+        #region ContainsAnyItem
+        /// <summary>
+        /// Returns whether or not this inventory has any item.
+        /// </summary>
+        public bool ContainsAnyItem()
+        {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i] != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        #region GetTotalInventoryLength
+        /// <summary>
+        /// Returns the total amount of items this inventory can hold (i.e. number of slots).
+        /// </summary>
+        public int GetTotalInventoryLength => inventory.Length;
+        #endregion
+
+        #region GetFirstAvailableSlotIndex
+        /// <summary>
+        /// Returns the first index of the slot that doesn't have any items in it.
+        /// </summary>
+        private int? GetFirstAvailableSlotIndex()
+        {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i].Count == 0)
+                {
+                    return i;
+                }
+            }
+            return null;
         }
         #endregion
 
@@ -159,10 +237,50 @@ namespace Utility.Inventory
 
         public IEnumerator<TItemBase> GetEnumerator()
         {
-            for (int i = 0; i < inventory.Count; i++)
+            for (int i = 0; i < inventory.Length; i++)
             {
                 yield return inventory[i][0];
             }
+        }
+        #endregion
+
+        #region OnItemAddedEventArgs
+        public class OnItemAddedEventArgs : System.EventArgs
+        {
+            #region Variables
+            public int AddedItemCount { get; }
+            public int IndexOfAddedItem { get; }
+            public TItemBase AddedItem { get; }
+            #endregion
+
+            #region Constructor
+            public OnItemAddedEventArgs(int addedItemCount, int indexOfAddedItem, TItemBase addedItem)
+            {
+                IndexOfAddedItem = indexOfAddedItem;
+                AddedItemCount = addedItemCount;
+                AddedItem = addedItem;
+            }
+            #endregion
+        }
+        #endregion
+
+        #region OnItemRemovedEventArgs
+        public class OnItemRemovedEventArgs : System.EventArgs
+        {
+            #region Variables
+            public int RemovedItemCount { get; }
+            public int IndexOfRemovedItem { get; }
+            public TItemBase RemovedItem { get; }
+            #endregion
+
+            #region Constructor
+            public OnItemRemovedEventArgs(int removedItemCount, int indexOfRemovedItem, TItemBase removedItem)
+            {
+                IndexOfRemovedItem = indexOfRemovedItem;
+                RemovedItemCount = removedItemCount;
+                RemovedItem = removedItem;
+            }
+            #endregion
         }
         #endregion
     }
