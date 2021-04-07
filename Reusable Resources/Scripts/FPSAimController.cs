e3874f7d23d9b29a7f5b9ab44996de1a98d46a1b
Utku Gokalp <38686664+UtkuGokalp@users.noreply.github.com>
2021-04-08 00:52:25 +0300

    Added a new FPSAimController.cs script for a reusable layer-based aiming behavior in FPS controller.

---
+++ Reusable Resources/Scripts/FPSAimController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using Utility.Development;
+
+namespace Utility.FPS
+{
+    public class FPSAimController : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField]
+        private LayerMask aimLayer;
+        private Transform aimedTransform;
+        public event TypeSafeEventHandler<FPSAimController, OnAimedEventArgs> OnAimed;
+        public event TypeSafeEventHandler<FPSAimController, OnAimLostEventArgs> OnAimIsLost;
+        #endregion
+
+        #region FixedUpdate
+        private void FixedUpdate()
+        {
+            Ray ray = Camera.main.ViewportPointToRay(Vector2.one * 0.5f);
+            bool hit = Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, aimLayer, QueryTriggerInteraction.Ignore);
+
+            if (hit)
+            {
+                if (aimedTransform == null)
+                {
+                    aimedTransform = hitInfo.transform;
+                    OnAimed?.Invoke(this, new OnAimedEventArgs(hitInfo));
+                }
+                else
+                {
+                    if (aimedTransform != hitInfo.transform)
+                    {
+                        OnAimIsLost?.Invoke(this, new OnAimLostEventArgs(aimedTransform));
+                        aimedTransform = hitInfo.transform;
+                        OnAimed?.Invoke(this, new OnAimedEventArgs(hitInfo));
+                    }
+                }
+            }
+            else
+            {
+                if (aimedTransform != null)
+                {
+                    OnAimIsLost?.Invoke(this, new OnAimLostEventArgs(aimedTransform));
+                    aimedTransform = null;
+                }
+            }
+        }
+        #endregion
+
+        #region OnAimedEventArgs
+        public class OnAimedEventArgs : System.EventArgs
+        {
+            public RaycastHit AimInfo { get; }
+
+            public OnAimedEventArgs(RaycastHit aimInfo)
+            {
+                AimInfo = aimInfo;
+            }
+        }
+        #endregion
+
+        #region OnAimLostEventArgs
+        public class OnAimLostEventArgs : System.EventArgs
+        {
+            public Transform TransformAimIsLostFrom { get; }
+
+            public OnAimLostEventArgs(Transform transformAimIsLostFrom)
+            {
+                TransformAimIsLostFrom = transformAimIsLostFrom;
+            }
+        }
+        #endregion
+    }
+}
