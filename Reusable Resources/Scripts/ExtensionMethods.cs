2c174ab6073d87f1a5cac5094737dddd68ae2539
UtkuGokalp <utkugokalp@gmail.com>
2021-11-02 14:51:08 +0300

    Update ExtensionMethods.cs
    Syntax error fix.

--- Reusable Resources/Scripts/ExtensionMethods.cs
+++ Reusable Resources/Scripts/ExtensionMethods.cs
@@ -68,10 +68,10 @@ namespace Utility.Development
         {
             return new Color()
             {
-                .r = 1 - color.r,
-                .g = 1 - color.g,
-                .b = 1 - color.b,
-                .a = reverseAlpha ? 1 - color.a : color.a,
+                r = 1 - color.r,
+                g = 1 - color.g,
+                b = 1 - color.b,
+                a = reverseAlpha ? 1 - color.a : color.a,
             };
         }
         #endregion
