284b21f06ceaf7fa1a9a3399aec204638dd126ba
UtkuGokalp <38686664+UtkuGokalp@users.noreply.github.com>
2024-06-29 14:49:45 +0300

    Updated Timer.cs so that it would support remaining and elapsed time.

--- Reusable Resources/Scripts/Timer.cs
+++ Reusable Resources/Scripts/Timer.cs
@@ -5,32 +5,50 @@ namespace Utility.Development
     public class Timer
     {
         #region Variables
+        private bool increasing;
         private float targetTime;
         private float currentTime;
         public event TypeSafeEventHandler<Timer, System.EventArgs>? OnTimerFinished;
+        public float ElapsedTime => increasing ? currentTime : targetTime - currentTime;
+        public float RemainingTime => increasing ? targetTime - currentTime : currentTime;
         #endregion
 
         #region Constructors
-        public Timer(float timeInSeconds)
+        public Timer(float timeInSeconds, bool increasing, TypeSafeEventHandler<Timer, System.EventArgs>? onTimerFinished)
         {
             targetTime = timeInSeconds;
-            OnTimerFinished = null;
-        }
-
-        public Timer(float timeInSeconds, TypeSafeEventHandler<Timer, System.EventArgs> onTimerFinished)
-        {
-            targetTime = timeInSeconds;
+            this.increasing = increasing;
             OnTimerFinished += onTimerFinished;
+
+            if (increasing)
+            {
+                currentTime = 0;
+            }
+            else
+            {
+                currentTime = targetTime;
+            }
         }
         #endregion
 
         #region Tick
         public void Tick(float elapsedTime)
         {
-            currentTime += elapsedTime;
-            if (currentTime > targetTime)
+            if (increasing)
             {
-                OnTimerFinished?.Invoke(this, System.EventArgs.Empty);
+                currentTime += elapsedTime;
+                if (currentTime >= targetTime)
+                {
+                    OnTimerFinished?.Invoke(this, System.EventArgs.Empty);
+                }
+            }
+            else
+            {
+                currentTime -= elapsedTime;
+                if (currentTime <= 0)
+                {
+                    OnTimerFinished?.Invoke(this, System.EventArgs.Empty);
+                }
             }
         }
         #endregion
